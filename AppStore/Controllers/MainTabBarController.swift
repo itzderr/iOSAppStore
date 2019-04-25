8e0a1a8884626b283b0486a3ecc60cf2b889e3fc
Derrick Park <derrick8689@gmail.com>
2019-04-25 15:36:49 -0700

    Initial Commit

---
+++ AppStore/Controllers/MainTabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  MainTabBarController.swift
+//  AppStore
+//
+//  Created by Derrick Park on 2019-04-25.
+//  Copyright © 2019 Derrick Park. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  
+    viewControllers = [
+      createViewController(viewController: UIViewController(), title: "Today", imageName: "today"),
+      createViewController(viewController: UIViewController(), title: "Games", imageName: "games"),
+      createViewController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+      createViewController(viewController: SearchViewController(), title: "Search", imageName: "search"),
+    ]
+  }
+  
+  fileprivate func createViewController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+    
+    viewController.view.backgroundColor = .white
+    viewController.navigationItem.title = title
+    let navController = UINavigationController(rootViewController: viewController)
+    navController.navigationBar.prefersLargeTitles = true
+    navController.tabBarItem.title = title
+    navController.tabBarItem.image = UIImage(named: imageName)
+    return navController
+  }
+}
---
+++ AppStore/Controllers/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  AppStore
+//
+//  Created by Derrick Park on 2019-04-25.
+//  Copyright © 2019 Derrick Park. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  
+  }
+}
